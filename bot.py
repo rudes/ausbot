7ddc29e9d6b072d468c03f4c336400446b8a8673
rudes <jacobriddle94@gmail.com>
2021-06-04 11:12:45 -0400

    log all the emotes

--- bot.py
+++ bot.py
@@ -41,13 +41,15 @@ class AusBot(discord.Client):
 			message = await channel.fetch_message(payload.message_id)
 			# this scans the message.reactions[] for a reaction that matches our star emote
 			await message.add_reaction(self.favorite_emote)
-			reaction = next((x for x in message.reactions if x.emoji == self.favorite_emote), None)
-			if reaction is None:
-				log.info('on_raw_reaction_add,message does not contain favorite_emote,{0}'.format(payload.message_id))
-			if reaction.count > 4:
-				await message.add_reaction(self.check_emote)
-				await message.attachments[0].save(self.file_storage+message.filename)
-				log.info('saved {0} to ausclan gallery'.format(message.filename))
+			#reaction = next((x for x in message.reactions if x.emoji == self.favorite_emote), None)
+			for reaction in message.reactions:
+				log.info('reaction: {}'.format(reaction.name))
+				if reaction is None:
+					log.info('on_raw_reaction_add,message does not contain favorite_emote,{0}'.format(payload.message_id))
+				if reaction.count > 4:
+					await message.add_reaction(self.check_emote)
+					await message.attachments[0].save(self.file_storage+message.filename)
+					log.info('saved {0} to ausclan gallery'.format(message.filename))
 		# handles exceptions for get_channel and fetch_message and save
 		except (discord.NotFound, discord.Forbidden, discord.HTTPException, discord.InvalidArgument) as e:
 			log.error('failed to pull file,{0}'.format(e.text))
