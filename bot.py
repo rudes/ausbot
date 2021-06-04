96c42d01b4b4d5e68c4f142f2e944fc0ff62d893
rudes <jacobriddle94@gmail.com>
2021-06-04 11:08:08 -0400

    double check star emote

--- bot.py
+++ bot.py
@@ -40,6 +40,7 @@ class AusBot(discord.Client):
 				return
 			message = await channel.fetch_message(payload.message_id)
 			# this scans the message.reactions[] for a reaction that matches our star emote
+			await message.add_reaction(self.favorite_emote)
 			reaction = next((x for x in message.reactions if x.emoji == self.favorite_emote), None)
 			if reaction is None:
 				log.info('on_raw_reaction_add,message does not contain favorite_emote,{0}'.format(payload.message_id))
